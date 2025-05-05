99e3f24452b25f016e623ec2d25479f0582cbd98
niedzwiedzislaw <kanngaxx@gmail.com>
2025-05-05 10:30:48 +0200

    Mógłem

--- translator/Translator.py
+++ translator/Translator.py
@@ -118,7 +118,11 @@ class Translator:
 
         def sg_2(): return root_sg + f"eś" + się if root_sg else ''
 
-        def sg_3(): return root_sg + f"" + się if root_sg else ''
+        def sg_3():
+            return (
+                    substitute(root_sg, 'mogł', 'mógł') or
+                    append(root_sg, '', '')
+            ) + się
 
         def pl_1(): return root_pl + f"śmy" + się if root_pl else ''
 
